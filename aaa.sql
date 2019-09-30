dc44032ff9f7bb56a97f236f896582189a172664
hb8520 <55868466+hb8520@users.noreply.github.com>
2019-09-30 11:04:12 +0800

    Delete aaa.sql

--- aaa.sql
+++
@@ -1,6 +0,0 @@
-Git is a version control system.
-Git is free software.
-bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
-111111111111111111111111111111
-222222222222222222222222222222
-cccccccccccccccccccccccccccccc
